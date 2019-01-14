8e9635c94c5ad5b320255e63a7504102054b830f
kevpy <kibetapps@gmail.com>
2019-01-14 21:18:18 +0300

    [Finishes #163144734] consume a specific user's orders

--- ui/js/parcels.js
+++ ui/js/parcels.js
@@ -72,5 +72,187 @@ const parcels = {
       .catch(err => {
         console.log("ERROR:", err.message);
       });
+  },
+
+  get_user_parcel: () => {
+    user_parcel_url =
+      base_uri + "users/" + `${localStorage.getItem("user_id")}/` + "parcels";
+    console.log(user_parcel_url);
+
+    fetch(user_parcel_url, {
+      mode: "cors",
+      method: "GET",
+      headers: h
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json().then(data => {
+            // window.location.href = "profile.html";
+            let len = Object.keys(data.Parcels).length;
+            let par = document.getElementById("all-user-parcels");
+            par.textContent += `${len}`;
+
+            data.Parcels.forEach(item => {
+              // console.log(item);
+              let div = document.createElement("div");
+              div.setAttribute("class", "test");
+              div.innerHTML = `
+                      <div>
+                        <div class="order-summary" id="order-id">${
+                          item.parcel_id
+                        }</div>
+                        <div class="order-summary" id="order-status">
+                            <span class="label other">${item.status}</span>
+                        </div>
+                        <div class="order-summary" id="order-location">${
+                          item.current_location
+                        }</div>
+                        <hr>
+                      </div>
+              `;
+              document.getElementById("all-user-orders").appendChild(div);
+            });
+          });
+        }
+        if (!response.ok) {
+          return response.json().then(data => {
+            for (const key of Object.keys(data)) {
+              let span = document.createElement("span");
+              span.setAttribute("class", "message");
+              span.innerHTML = `
+                    <strong>${key}</strong> : <em>${data[key]}</em><br>
+              `;
+              document.getElementById("notification").appendChild(span);
+            }
+          });
+        }
+      })
+      .catch(err => {
+        console.log("ERROR:", err.message);
+      });
+  },
+
+  get_on_transit_parcel: () => {
+    user_parcel_url =
+      base_uri + "users/" + `${localStorage.getItem("user_id")}/` + "parcels";
+    console.log(user_parcel_url);
+
+    fetch(user_parcel_url, {
+      mode: "cors",
+      method: "GET",
+      headers: h
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json().then(data => {
+            // window.location.href = "profile.html";
+            let len = 0;
+
+            data.Parcels.forEach(item => {
+              if (item.status == "pending delivery") {
+                len++;
+                console.log(item);
+                let div = document.createElement("div");
+                div.setAttribute("class", "test");
+                div.innerHTML = `
+                      <div>
+                        <div class="order-summary" id="order-id">${
+                          item.parcel_id
+                        }</div>
+                        <div class="order-summary" id="order-status">
+                            <span class="label other">${item.status}</span>
+                        </div>
+                        <div class="order-summary" id="order-location">${
+                          item.current_location
+                        }</div>
+                        <hr>
+                      </div>
+              `;
+                document.getElementById("pd-user-orders").appendChild(div);
+              }
+            });
+            let par = document.getElementById("pd-user-parcels");
+            par.textContent += `${len}`;
+          });
+        }
+        if (!response.ok) {
+          return response.json().then(data => {
+            for (const key of Object.keys(data)) {
+              let span = document.createElement("span");
+              span.setAttribute("class", "message");
+              span.innerHTML = `
+                    <strong>${key}</strong> : <em>${data[key]}</em><br>
+              `;
+              document.getElementById("notification").appendChild(span);
+            }
+          });
+        }
+      })
+      .catch(err => {
+        console.log("ERROR:", err.message);
+      });
+  },
+
+  get_delivered_parcels: () => {
+    user_parcel_url =
+      base_uri + "users/" + `${localStorage.getItem("user_id")}/` + "parcels";
+    console.log(user_parcel_url);
+
+    fetch(user_parcel_url, {
+      mode: "cors",
+      method: "GET",
+      headers: h
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json().then(data => {
+            // window.location.href = "profile.html";
+            let len = 0;
+
+            data.Parcels.forEach(item => {
+              if (item.status == "delivered") {
+                len++;
+                console.log(item);
+                let div = document.createElement("div");
+                div.setAttribute("class", "test");
+                div.innerHTML = `
+                      <div>
+                        <div class="order-summary" id="order-id">${
+                          item.parcel_id
+                        }</div>
+                        <div class="order-summary" id="order-status">
+                            <span class="label success">${item.status}</span>
+                        </div>
+                        <div class="order-summary" id="order-location">${
+                          item.current_location
+                        }</div>
+                        <hr>
+                      </div>
+              `;
+                document
+                  .getElementById("delivered-user-orders")
+                  .appendChild(div);
+              }
+            });
+            let par = document.getElementById("delivered-user-parcels");
+            par.textContent += `${len}`;
+          });
+        }
+        if (!response.ok) {
+          return response.json().then(data => {
+            for (const key of Object.keys(data)) {
+              let span = document.createElement("span");
+              span.setAttribute("class", "message");
+              span.innerHTML = `
+                    <strong>${key}</strong> : <em>${data[key]}</em><br>
+              `;
+              document.getElementById("notification").appendChild(span);
+            }
+          });
+        }
+      })
+      .catch(err => {
+        console.log("ERROR:", err.message);
+      });
   }
 };
